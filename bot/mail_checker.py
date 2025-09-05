f6f8dd4575fd86d7899622d61b810437726fa16b
Maks-Y <68940343+Maks-Y@users.noreply.github.com>
2025-09-05 16:56:59 +0300

    Add integration test for mail checker

--- bot/mail_checker.py
+++ bot/mail_checker.py
@@ -18,10 +18,14 @@ async def start_mail_checker():
                 for num in data[0].split():
                     typ, msg_data = M.fetch(num, "(RFC822)")
                     msg = email.message_from_bytes(msg_data[0][1])
+                    msg_id = msg.get("Message-ID", num.decode())
+                    logging.info("Processing mail %s", msg_id)
                     fio, date = parse_hr_mail(msg)
                     if fio and date:
                         run_dt = date.replace(hour=16, minute=0, second=0, tzinfo=TZ)
                         schedule_disable_job(fio, run_dt, created_by=0, meta={"source": "mail"})
+                        M.store(num, "+FLAGS", "\\Seen")
+                        logging.info("Processed mail %s", msg_id)
         except Exception:
             logging.exception("IMAP poll error")
         await asyncio.sleep(int(os.getenv("IMAP_POLL_SECONDS", "300")))
